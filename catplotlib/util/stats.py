ae27c527932bf3c69d81fc8adaafafa92684950a
mfellows <mfellows@nrcan.gc.ca>
2023-12-07 09:55:13 -0800

    Documentation, multiprocessing for calculate_stack_stat.

--- catplotlib/util/stats.py
+++ catplotlib/util/stats.py
@@ -2,11 +2,12 @@ import logging
 import warnings
 import numpy as np
 from pathlib import Path
+from multiprocessing import Pool
 from catplotlib.util.config import gdal_memory_limit
 from mojadata.util import gdal
-from mojadata.util.gdalhelper import GDALHelper
 from catplotlib.spatial.layer import Layer
 from catplotlib.spatial.boundingbox import BoundingBox
+from catplotlib.util.cache import get_cache
 
 def _find_layers(pattern, pattern_filter_fn=None):
     initial_path = "."
@@ -19,14 +20,22 @@ def _find_layers(pattern, pattern_filter_fn=None):
     
     return input_layers
 
-def _normalize_layers(layer_paths):
-    n_original_layers = len(layer_paths)
-    bounding_box = BoundingBox(str(layer_paths[0]))
+def _normalize_layers(layer_paths, cache):
+    bounding_box = BoundingBox(str(layer_paths[0]), cache=cache)
     normalized_input_layers = []
-    for i, path in enumerate(layer_paths, 1):
-        logging.info(f"    layer {i} / {n_original_layers}")
-        normalized_layer = bounding_box.crop(Layer(str(path), 0), False)
-        normalized_input_layers.append(normalized_layer.path)
+    with Pool() as pool:
+        tasks = []
+        for layer_path in layer_paths:
+            tasks.append(pool.apply_async(
+                bounding_box.crop,
+                (Layer(str(layer_path), 0, cache=cache), False)
+            ))
+        
+        pool.close()
+        pool.join()
+        
+        for result in tasks:
+            normalized_input_layers.append(result.get().path)
 
     return normalized_input_layers
 
@@ -34,17 +43,38 @@ def calculate_stack_stat(
     pattern, output_path, numpy_fn, *numpy_args, pattern_filter_fn=None, chunk_size=5000,
     **numpy_kwargs
 ):
+    '''
+    Performs a numpy operation per pixel (in space) on a stack of rasters and writes the
+    result to a single new raster - for example, the per-pixel 95th percentile value.
+
+    Arguments:
+    'pattern' -- the file pattern to search for
+    'output_path' -- the path to the output raster to create
+    'numpy_fn' -- a stack-compatible numpy function to use, i.e. sum, percentile, nanpercentile
+    'numpy_args' -- positional args to pass to the numpy function
+    'pattern_filter_fn' -- an additional filter function to determine if each layer
+        found by the search pattern should be included or not; must be a function that
+        takes a single arg, a file path, and returns True or False
+    'chunk_size' -- the size of the chunks to process
+    'numpy_kwargs' -- keyword args to pass to the numpy function
+    '''
     logging.info(f"Processing {pattern}...")
+
     gdal.SetCacheMax(gdal_memory_limit)
+    cache = get_cache()
     Path(output_path).unlink(True)
     
     input_layers = _find_layers(pattern, pattern_filter_fn)
+    if not input_layers:
+        logging.error("  no layers found")
+        return
+
     logging.info("  normalizing layers to same extent and resolution")
-    normalized_input_layers = _normalize_layers(input_layers)
+    normalized_input_layers = _normalize_layers(input_layers, cache)
     n_layers = len(normalized_input_layers)
     logging.info(f"  processing {n_layers} normalized layers")
 
-    output_template = Layer(str(normalized_input_layers[0]), 0)
+    output_template = Layer(str(normalized_input_layers[0]), 0, cache=cache)
     output_template.blank_copy(str(output_path))
     ndv = output_template.nodata_value
     output_raster = gdal.Open(str(output_path), gdal.GA_Update)
